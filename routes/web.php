a3ddc3723bc7d230d3e646b6bf2b68b37157aa1d
Ubuntu <ubuntu@ip-172-31-23-29.ap-northeast-1.compute.internal>
2022-09-30 08:50:06 +0000

    todolist controller model migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodolistsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Route::get('/', [TodolistsController::class,'index']);
+
+// Route::resource('Todolists', 'TodolistsController');
 
 Route::get('/', function () {
     return view('welcome');
